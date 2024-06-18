8e8157d85a1d337664b1675d3ccb9dea23e63b4d
Levi Qin <levi.qin@Easeware.org>
2024-06-18 09:29:41 +0800

    源码提交

--- src/utils/judgmentEnv.js
+++ src/utils/judgmentEnv.js
@@ -5,5 +5,5 @@
 export const getBaseURL = () => {
     const HOST = window.location.host;
     const PROD = "leviqin.top"; // 生产环境
-    return HOST === PROD ? `https://api.leviqin.top` : `http://localhost:3000`;
+    return HOST.includes(PROD) ? `https://api.leviqin.top` : `http://localhost:3000`;
 }
